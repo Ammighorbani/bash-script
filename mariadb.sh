4307537170949c3075d99f5fdf055d9f524a20bd
Ammighorbani <118529478+Ammighorbani@users.noreply.github.com>
2024-05-30 21:31:31 +0330

    mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -8,12 +8,15 @@ sudo apt upgrade && sudo apt install mariadb-server -y
 # This command will open secure installation menu for you
 sudo mariadb-secure-installation
 
+# This three lines will give your user-name, user-pass and your host ip
 
 read -p "Enter your user: " USER
 read -s -p "Enter your password: " PASS
 read -p "Enter your host name: " HOST 
 
+# NOTE: If you are runing this file into your server you can write "localhost" for HOST
 
+# Creating db
 
 sudo mariadb -u$USER -p$PASS -h$HOST -e"SHOW DATABASES;" && read -p "Create database: " Cdb &&
        	sudo mariadb -u$USER -e "$Cdb" && sudo mariadb -u$USER -e "SHOW DATABASES;" &&
