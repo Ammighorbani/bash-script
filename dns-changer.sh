db001c42865d10844c3bec8379cab04b72138a75
Ammighorbani <118529478+Ammighorbani@users.noreply.github.com>
2024-08-09 17:32:30 +0330

    Correction

--- dns-changer.sh
+++ dns-changer.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # This script will change your nameserver in /etc/resolv.conf
-# First touch a file and write this nameservers in it
-# nameserver 178.22.122.100
-# nameserver 185.51.200.2
-# nameserver 185.55.226.26
-# nameserver 185.55.225.25
 
-# Write this file PATH into .profile and use "&" after that
-# example: /root/scripts/dnsch&
+
+sudo echo "nameserver 178.22.122.100" > /root/dns
+sudo echo "nameserver 185.51.200.2" >> /root/dns
+sudo echo "nameserver 185.55.226.26" >> /root/dns
+sudo echo "nameserver 185.55.225.25" >> /root/dns
+
+echo /root/dns& > cpprofile
+cat .profile >> cpprofile
+cat cpprofile > .profile
 
 
 for (( ; ; ))
 do 
-      sudo cat ["nameserver-file"] > /etc/resolv.conf
+      sudo cat /root/dns > /etc/resolv.conf
       sleep 180
+      echo "" > /root/dns
 
 done
 
